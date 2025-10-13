0848db6bb2c10e8affd1022a3a25e68dcd1854ba
Termsmh <marco.malik17@gmail.com>
2025-10-13 09:51:01 +0200

    Implementoval Start hry a Exit hry

---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void SwitchToArenaScene()
+    {
+        SceneManager.LoadScene("xHamsters");
+    }
+    public void ExitGame()
+    {
+        Application.Quit(); // pokud hrajes v buildu
+
+        UnityEditor.EditorApplication.isPlaying = false; // pokud hrajes v editoru (Play Mode)
+    }
+}
